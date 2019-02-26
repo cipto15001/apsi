e6a263721104b3167ac7a7b63a6aeb2544bf584a
Cipto Tri Utomo <ciptredevie@gmail.com>
2019-02-26 18:43:39 +0700

    add simulation packet choice using gui, template and editor

--- resources/views/jobs/create_empty.blade.php
+++ resources/views/jobs/create_empty.blade.php
@@ -8,20 +8,41 @@
             </center>
         </div>
         @foreach (\App\Simulation::with('upload')->get() as $simulation)
-            <div class="mdl-cell mdl-cell--6-col">
-                <center>
-                    <div class="menu-card-square mdl-card mdl-shadow--2dp" style="background: none; box-shadow: none; border: nonekk">
-                        <img src="{{ $simulation->image }}" alt="">
-                        Image Placeholder : {{ $simulation->image }}
-                        <br>
-                        Description : {{ $simulation->description }}
-                    </div>
-                    <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-                       href="{{ route('workspaces.jobs.edit_template', [$workspace, $simulation->slug]) }}">
-                        {{ $simulation->title }}
-                    </a>
-                </center>
+            <div class="col-md-3 col-sm-12 col-lg-3">
+                    <div class="card">
+                            <div class="header">
+                                <img class="card-img-top" src="{{ asset('img/water.png') }}" alt="Card image cap">
+                            </div>
+                            <div class="body bg-amber">
+                                <h4>SPH</h4>
+                                <p class="align-left">{{ $simulation->description }}</p>
+                            </div>
+                            <div class="body bg-amber p-t--0 ">
+                                <a class="p-t-0" href="{{ route('workspaces.jobs.edit_template', [$workspace, $simulation->slug]) }}" target="_blank">
+                                    <button type="button" class="btn bg-indigo waves-effect" data-toggle="tooltip" data-placement="top" title="Write Job With Template">
+                                        <i class="material-icons">dvr</i>
+                                    </button>
+                                </a>
+                                <a href="{{ route('workspaces.jobs.edit_template', [$workspace, $simulation->slug]) }}" target="_blank">
+                                    <button type="button" class="btn bg-indigo  waves-effect" data-toggle="tooltip" data-placement="top" title="Write Job With Editor">
+                                        <i class="material-icons">edit</i>
+                                    </button>
+                                </a>
+                                <a href="{{ route('workspaces.jobs.edit_template', [$workspace, $simulation->slug]) }}" target="_blank">
+                                    <button type="button" class="btn bg-indigo waves-effect" data-toggle="tooltip" data-placement="top" title="Write Job With GUI">
+                                        <i class="material-icons">touch_app</i>
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
             </div>
         @endforeach
     </div>
 @endsection
+@push('scripts')
+    <script>
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
+@endpush
